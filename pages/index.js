1a767e445e652c782706247566090aa57f600cc2
Taiany Brazil <69211964+braziltaiany@users.noreply.github.com>
2024-12-12 22:27:03 +0000

    add page

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <div><h2>Olá, Claudia!</h2> <h3> Minha amiga das risadas incontroláveis! 🌟<br/> Que nossa amizade continue a brilhar forte, com muitas risadas e aventuras. <br/>Você é uma amiga para a vida toda, e sou muito feliz por termos crescido juntas! 🐱💖</h3></div>
+}
+
+export default Home;
