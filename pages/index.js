1af0ffe38b68122d0f9bee4345a8513985cac6fc
Taiany Brazil <69211964+braziltaiany@users.noreply.github.com>
2025-01-15 14:54:04 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <div><h2>Olá, Claudia!</h2> <h3> Minha amiga das risadas incontroláveis! 🌟<br/> Que nossa amizade continue a brilhar forte, com muitas risadas e aventuras. <br/>Você é uma amiga para a vida toda, e sou muito feliz por termos crescido juntas! 🐱💖</h3></div>
+  return (
+    <div>
+      <h2> Olá, mundo! </h2>
+    </div>
+  );
 }
 
 export default Home;
